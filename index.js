66766baf5176987baf6820262a848d5eae70c248
Robin-Milford <robinmilford38@gmail.com>
2021-03-30 13:23:16 +0600

    add root api

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.get('/',(req,res)=>{
+    res.send('Server working successfully')
+})
+
 
 
 client.connect(err => {
